134dd2e2cb955e53f86c1a7edf154ad11f26e90e
PWFoox <pwfoox@mail.com>
2024-12-20 03:46:41 +0500

    modified:   6.cpp

--- 6.cpp
+++ 6.cpp
@@ -6,7 +6,7 @@ int main()
 {
     cout << "Hello  world\n";
     cout << "Привет как дела\n";
-    int a = 50, b = 60;
-    cout << a + b << endl;
+    int a = 50, b = 60, c = 100;
+    cout << a + b + c << endl;
     cout << a - b;
 }
